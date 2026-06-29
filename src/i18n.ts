0da041261151afded892eec997ccbaaed3bfac6b
n20186005-hash <n20186005@gmail.com>
2026-06-29 21:29:40 +0800

    更新

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,5 +1,14 @@
 import {getRequestConfig} from 'next-intl/server';
+import {notFound} from 'next/navigation';
 
-export default getRequestConfig(async ({locale}) => ({
-  messages: (await import(`../messages/${locale}.json`)).default
-}));
+const locales = ['it', 'en', 'de', 'zh-Hant'];
+
+export default getRequestConfig(async ({locale}) => {
+  if (!locales.includes(locale)) {
+    notFound();
+  }
+
+  return {
+    messages: (await import(`../messages/${locale}.json`)).default
+  };
+});
